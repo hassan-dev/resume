657eab7b9610d64716456ff4a1d715867216ec29
unknown <itsgardezi@gmail.com>
2019-11-02 01:10:25 +0500

    Updates

---
+++ precache-manifest.e98270d271ae9e9354d7eed86c50b6f9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0a0c00c4199d1bf44b90650addcabcd",
+    "url": "/resume1/index.html"
+  },
+  {
+    "revision": "78f7bcca9a2e7c1f8465",
+    "url": "/resume1/static/css/main.5b1d244b.chunk.css"
+  },
+  {
+    "revision": "de69dfad5acb0b20451d",
+    "url": "/resume1/static/js/2.e87076d8.chunk.js"
+  },
+  {
+    "revision": "78f7bcca9a2e7c1f8465",
+    "url": "/resume1/static/js/main.a4acf8c3.chunk.js"
+  },
+  {
+    "revision": "bf450c8f4793a1c1c834",
+    "url": "/resume1/static/js/runtime-main.cc5d6d92.js"
+  },
+  {
+    "revision": "d17600ade282443621aab6b90427cd35",
+    "url": "/resume1/static/media/002.d17600ad.jpg"
+  },
+  {
+    "revision": "dc68abb40b116f55b94dc3e35375111d",
+    "url": "/resume1/static/media/004.dc68abb4.jpg"
+  },
+  {
+    "revision": "dd7024b933069f96e2c7452c39e01de7",
+    "url": "/resume1/static/media/005.dd7024b9.jpg"
+  }
+]);
